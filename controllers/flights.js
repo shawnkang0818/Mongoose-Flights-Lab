7c193ed0167c83dbde19bd842d35ff72bf406095
Shawn Kang <shawnkang0818@gmail.com>
2024-01-17 06:11:13 -0500

    add flight list

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,4 +1,4 @@
-
+import { Flight } from "../models/flight.js"
 
 function newFlight(req,res){
   res.render("flights/new", {
@@ -6,6 +6,37 @@ function newFlight(req,res){
   })
 }
 
+function create(req, res){
+  if (req.body.cast) {
+		// remove whitespace next to commas
+    req.body.cast = req.body.cast.split(', ')
+  }
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+    flights: flights,
+    title: 'All Flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
-  newFlight as new
+  newFlight as new,
+  create,
+  index
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,8 +3,12 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
+// GET localhost:3000/flghts
+router.get('/', flightsCtrl.index)
 // GET localhost:3000/flghts/new
 router.get('/new', flightsCtrl.new)
+// GET localhost:3000/flghts/
+router.post('/', flightsCtrl.create)
 
 export { 
   router
